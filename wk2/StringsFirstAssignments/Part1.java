fff0b11d9b7eeb45b9ea5575b53e22883eadd576
CodeZaru <teamsullivank@gmail.com>
2018-04-27 19:33:28 -0400

    files

--- wk2/StringsFirstAssignments/Part1.java
+++ wk2/StringsFirstAssignments/Part1.java
@@ -6,7 +6,31 @@
  * @version (a version number or a date)
  */
 public class Part1 {
+    
+    /**
+     *
+     */
+    public String findSimpleGene(String dna) {
+        // Finds the index position of the start/stop codons “ATG”/"TAA. If either is missing, 
+        //returns the empty string, If both exist and substring is multiple of 3, then return substring.
+        String result = "";
+        int startIndex = dna.indexOf("ATG");
+        int stopIndex = dna.indexOf("TAA", startIndex+3);
+        result = dna.substring(startIndex, stopIndex+3);
+        
+        return result;
+    }
 
+    public void testSimpleGene(){
+        
+        String dna = "";
+        String gene = "";
+                
+        dna = "ATCCTATGCTTCGGCTGCTCAATATGGT";
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+    }
     
     
 }
