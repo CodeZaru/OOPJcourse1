5c5ef029d3714609252364772f6bd291338ebc8d
CodeZaru <teamsullivank@gmail.com>
2018-04-27 20:26:48 -0400

    files

--- wk2/StringsFirstAssignments/Part1.java
+++ wk2/StringsFirstAssignments/Part1.java
@@ -27,7 +27,7 @@ public class Part1 {
         result = dna.substring(startIndex, stopIndex+3);
         if(result.length() % 3 != 0)
         {
-            return "";
+            return "has start/stop codons, but isn't multiple of three";
         }
         return result;
     }
@@ -42,17 +42,21 @@ public class Part1 {
         gene = findSimpleGene(dna);
         System.out.println("Gene is " + gene);
                        
-        dna = "ATCCTTGCTTCGGCTGCTCTAATATGGT";//no startCodon
+        dna = "ATCCTTGCTTCGGCTGCTCTAATATGGT";//no ATG startCodon
         System.out.println("DNA strand is (no startCodon) " + dna);
         gene = findSimpleGene(dna);
         System.out.println("Gene is " + gene);
     
-        dna = "ATCCTATGCTTCGGCTGCTCAATATGGT";//no stopCodon
+        dna = "ATCCTATGCTTCGGCTGCTCAATATGGT";//no TAA stopCodon
         System.out.println("DNA strand is  (no stopCodon) " + dna);
         gene = findSimpleGene(dna);
         System.out.println("Gene is " + gene);
    
-    
+        dna = "ATCCTATGTTCGGCTGCTCTAATATGGT";//not a multiple of 3
+        System.out.println("DNA strand is " + dna);
+        gene = findSimpleGene(dna);
+        System.out.println("Gene is " + gene);
+                       
     
     }
     
