6a182f2e602211d33dd2392cf7ce721bdc55a6fa
CodeZaru <teamsullivank@gmail.com>
2018-04-28 01:23:37 -0400

    files

--- wk2/StringsFirstAssignments/Part4.java
+++ wk2/StringsFirstAssignments/Part4.java
@@ -1,12 +1,31 @@
 
 /**
  * Write a description of Part4 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (kevinSullivan) 
+ * @version (4-27-18)
  */
+
+import edu.duke.*;
+
 public class Part4 {
 
+    public void findAndPrintUrls(String url) {
+        URLResource targetUrl = new URLResource(url);
+        for(String word : targetUrl.words()) {
+         if(word.toLowerCase().indexOf("youtube.com") != -1) {
+             int argIndexStart = word.indexOf("\"");
+             int argIndexEnd = word.indexOf("\"",argIndexStart+1);
+             System.out.println(word.substring(argIndexStart+1,argIndexEnd)); 
+            }
+        }
+        
+    }
+    
+    public void testFindAndPrintUrls() {
+     findAndPrintUrls("http://www.dukelearntoprogram.com/course2/data/manylinks.html");   
+      
+    }
+    
     
     
     
